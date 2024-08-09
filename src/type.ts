af41406215e6a09b2a2f3405d47817bc807ffc1a
JiafeiAKA <153718860+JiafeiAKA@users.noreply.github.com>
2024-08-09 12:43:06 +0700

    init

---
+++ src/type.ts
@@ -0,0 +1,32 @@
+export interface MessageState{
+    message: string
+}
+
+export interface EventState {
+    event: Event | null
+}
+
+export interface Event {
+    id: number
+    category: string
+    title: string
+    description: string
+    location: string
+    date: string
+    time: string
+    petsAllowed: boolean
+    organizer: string
+}
+
+export interface Student {
+    id: number
+    studentId: string
+    name: string
+    surname: string
+    gpa: number
+    image: string
+    penAmount: number
+    description: string
+  }
+
+  
